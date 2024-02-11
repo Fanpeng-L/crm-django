59a36e9e0752cab7427e27d1b9f0b1262e98886a
Fanpeng Li <serforfp@gmail.com>
2024-02-11 18:15:55 +0000

    Created templates

--- studyRoom/base/views.py
+++ studyRoom/base/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-# Create your views here.
+# Create your views here
+
+rooms = [
+    {"id": 1, "name": "Let's learn drawing"},
+    {"id": 2, "name": "Designing together"},
+    {"id": 3, "name": "Let's start coding"},
+]
+
+
 def home(request):
-    return HttpResponse("Home page")
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
 
-def room(request):
-    return HttpResponse("ROOM")
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+    context = {"room": room}
+    return render(request, "base/room.html", context)
